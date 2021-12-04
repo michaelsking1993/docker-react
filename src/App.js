343676d9338130ea300ef39192769ac9aca5a0a0
MICHAEL KING <michaelsking1993@gmail.com>
2021-12-04 12:07:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is <code>Codie Mcgee</code>. Hear me roar.
         </p>
         <a
           className="App-link"
